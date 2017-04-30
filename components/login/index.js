2f24bb7be229ca73e232d33dd56864c50655727b
Santosh Venkatraman <santosh.venk@gmail.com>
2017-05-01 03:06:11 +0530

    Cleans up Login component

--- components/login/index.js
+++ components/login/index.js
@@ -1,40 +1,18 @@
 import React from 'react';
 import {
-  AppRegistry,
   Text,
   TouchableOpacity
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 
-import HomeScreen from '../home';
-
-class LoginScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Login',
-  };
-  render() {
-    console.log('props', this.props);
-    console.log('navigation props', this.props.navigation);
-    const { navigate } = this.props.navigation;
-    return (
-      <TouchableOpacity onPress={ () => navigate('Home') }>
-        <Text>
-          Click to login!
-        </Text>
-      </TouchableOpacity>
-    );
-  }
-}
-
-const AppRouteConfigs = {
-  Login: {
-    screen: LoginScreen
-  },
-  Home: {
-    screen: HomeScreen
-  }
+const Login = ({ goHome }) => {
+  return (
+    <TouchableOpacity onPress={ goHome }>
+      <Text>
+        Click to login!
+      </Text>
+    </TouchableOpacity>
+  );
 };
-const AppNavigator = StackNavigator(AppRouteConfigs);
 
-export default AppNavigator;
+export default Login;
